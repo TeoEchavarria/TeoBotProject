e56ccebabb9bbe2753023581bce0773d86f59e84
TeoEchavarria <echavarria.0042@gmail.com>
2023-10-16 17:09:29 -0500

    Update Project

--- models/teoBot_model.py
+++ models/teoBot_model.py
@@ -3,13 +3,8 @@ import streamlit as st
 
 class TeoBotModel:
     def __init__(self):
-        self.llm = AutoModelForCausalLM.from_pretrained(
-            "TheBloke/Mistral-7B-Instruct-v0.1-GGUF", 
-            model_file="mistral-7b-instruct-v0.1.Q4_K_S.gguf", 
-            model_type="mistral", 
-            gpu_layers=40
-            )
+        pass
      
     def generate_response(self, question):
-        return self.llm(question)
+        pass
 
