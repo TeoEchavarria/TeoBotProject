ab6f97ed25ba5a43419a46d0d29f108341946072
TeoEchavarria <echavarria.0042@gmail.com>
2023-10-16 15:29:22 -0500

    Update Model and Requirements

--- models/teoBot_model.py
+++ models/teoBot_model.py
@@ -5,7 +5,7 @@ class TeoBotModel:
     def __init__(self):
         self.llm = AutoModelForCausalLM.from_pretrained(
             "TheBloke/Mistral-7B-Instruct-v0.1-GGUF", 
-            model_file="mistral-7b-instruct-v0.1.Q2_K.gguf", 
+            model_file="mistral-7b-instruct-v0.1.Q4_K_S.gguf", 
             model_type="mistral", 
             gpu_layers=40
             )
