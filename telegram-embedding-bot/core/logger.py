3318389cf35c493170be079fb615ee7023352433
TeoEchavarria <echavarria.0042@gmail.com>
2024-09-02 15:07:52 -0500

    Logger

---
+++ telegram-embedding-bot/core/logger.py
@@ -0,0 +1,16 @@
+import logging
+
+class LoggingUtil:
+    """
+    Util to create logs in console
+    """
+
+    @classmethod
+    def setup_logger(cls):
+        logger = logging.getLogger('telegram-embedding-bot')
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+        return logger
