e4fe3f02f83c53306064d269e93535dc121be44b
TeoEchavarria <echavarria.0042@gmail.com>
2024-09-07 13:24:05 -0500

    Basic Response

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from src.document_processing.loader import process_markdown_files, process_data_embeddings
 from src.embeddings.pinecone import upsert_embeddings_to_pinecone
 from src.embeddings.search import search
+from src.bot.response import generate_answer
 from dotenv import load_dotenv
 from src.bot.bot import main
 
@@ -12,4 +13,7 @@ if __name__ == "__main__":
     #process_data_embeddings()
     #upsert_embeddings_to_pinecone()
     #print(search("Predicciones con datos a travez del tiempo"))
-    main()
+    #main()
+    
+    output = generate_answer("The capital of France is Paris.", "What is the capital of France?")
+    print(output)
