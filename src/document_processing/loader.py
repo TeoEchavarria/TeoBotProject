25b50ef593bc7c5a62542121de2e0d3cd056fb3e
TeoEchavarria <echavarria.0042@gmail.com>
2024-11-11 22:33:51 -0500

    Refactor file path for embedding search functionality and improve message handling in start command

--- src/document_processing/loader.py
+++ src/document_processing/loader.py
@@ -1,8 +1,11 @@
 import os
 import requests
 from src.utils.mongodb import insert, find_one, update_one, collection
-from src.services.embeddings.embedder import get_embedding_from_markdown
+from src.services.embeddings.embedder import get_embedding
 from src.services.github.github_extract import get_github_directory_files
+from src.core.logger import LoggingUtil
+
+logger = LoggingUtil.setup_logger()
 
 def download_file_content(url):
     response = requests.get(url)
@@ -11,7 +14,7 @@ def download_file_content(url):
     else:
         raise Exception(f"Failed to download file from {url}")
 
-def update_markdown_files(user_name, files = None):
+async def update_markdown_files(context, update, user_name, files = None):
     from src.utils.mongodb import find_one, upsert, create_db_and_collection
     user = find_one("users", {"_id": user_name})
     if user is None:
@@ -20,31 +23,46 @@ def update_markdown_files(user_name, files = None):
         create_db_and_collection(user["mongo_key"], "Notes_bot", "notes")
         if not files:
             files = get_github_directory_files(user["owner"], user["repo"], user["directory_path"])
-        process_markdown_files(files)
+        await process_markdown_files(context, update, files)
         return files
     except Exception as e:
-        return "Error updating markdown files"
+        logger.error(f"Error updating markdown files: {e}")
+        raise Exception("Error updating markdown files")
 
-def process_markdown_files(files, mongo_key = None):
+async def process_markdown_files(context, update, files, mongo_key = None):
+    count = 0
     for file_name, url_content in files.items():
         content = download_file_content(url_content)
         content = content.replace('#', '').replace("\n", " ").replace("\\", "").replace("[[" , "").replace("]]", "")
         existing_document = find_one("notes", {"_id": file_name}, mongo_key)
+        embedding = get_embedding(content)
         if existing_document:
             updated = False
-            if existing_document["url"] != file_name or existing_document["content"] != content:
+            if existing_document["_id"] != file_name or existing_document["content"] != content:
                 updated = True
             update_data = {
                 "_id": file_name,
                 "content": content,
-                "updated": not updated
+                "updated": not updated,
+                "embedding_content": embedding
             }
             update_one("notes", {"_id": file_name}, update_data)  
         else:
             data = {
                 "_id": file_name,
                 "content": content,
-                "embedding_content": None,
+                "embedding_content": embedding,
                 "updated": False
             }
             insert("notes", data) 
+        count += 1
+        if count == 1:
+            message = await update.message.reply_text('1 File updated')
+            context.user_data['message_id'] = message.message_id
+            if context.user_data.get('message_id') is None:
+                context.user_data['message_id'] = message.id
+        else:
+            chat_id = update.effective_chat.id
+            message_id = context.user_data.get('message_id')
+            message_text = f"{count} Files updated"
+            await context.bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=message_text)
--- src/services/embeddings/embedder.py
+++ src/services/embeddings/embedder.py
@@ -8,22 +8,3 @@ def get_embedding(text):
     # Create the embedding using the OpenAI API
     embedding = openai.embeddings.create(input=text, model="text-embedding-3-large")
     return embedding.data[0].embedding
-
-def get_embedding_from_markdown(element):
-    # Fetch the content from the Markdown URL
-    url = element["url"]
-    content = element["content"]
-
-    # Create the embedding using the OpenAI API
-    embedding_content = openai.embeddings.create(input=content, model="text-embedding-3-large")
-
-    # Access the embedding vectors
-    embedding_vector_content = embedding_content.data[0].embedding
-
-    # Return the JSON structure
-    return {
-        "url": url,
-        "content": content,
-        "embedding_content": embedding_vector_content,
-        "updated": True
-    }
